95a22d1ebd8b4007fc7df753136db9d27fb668fc
Shubh Shubhanjal <shubhshubhanjal96@gmail.com>
2025-02-03 14:02:57 +0530

    Implement user registration and login functionality with MongoDB integration

---
+++ Backend/routes/user.routes.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const router = express.Router();
+const { body } = require("express-validator");
+const userController = require("../controllers/user.controller");
+
+router.post(
+  "/register",
+  [
+    body("email").isEmail().withMessage("Invalid Email"),
+    body("fullName.firstName")
+      .isLength({ min: 3 })
+      .withMessage("First name should be minimum of 3 characters long."),
+    body("password")
+      .isLength({ min: 6 })
+      .withMessage("Password must be of atleast 6 characters."),
+  ],
+  userController.registerUser
+);
+
+router.post(
+  "/login",
+  [
+    body("email").isEmail().withMessage("Invalid Email"),
+    body("password")
+      .isLength({ min: 6 })
+      .withMessage("Password must be of atleast 6 characters."),
+  ],
+  userController.loginUser
+);
+
+module.exports = router;
